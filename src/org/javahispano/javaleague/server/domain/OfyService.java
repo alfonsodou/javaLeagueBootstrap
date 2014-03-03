6b1269325a8e3e7dfb02b40d96c5a5ec09f90d49
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2014-03-03 15:34:42 +0100

    Creación OfyService

---
+++ src/org/javahispano/javaleague/server/domain/OfyService.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.javahispano.javaleague.server.domain;
+
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyFactory;
+import com.googlecode.objectify.ObjectifyService;
+
+/**
+ * @author adou
+ *
+ */
+public class OfyService {
+    static {
+        factory().register(CalendarDate.class);
+        factory().register(FrameWork.class);
+        factory().register(League.class);
+        factory().register(Match.class);
+        factory().register(PartidoStore.class);
+        factory().register(TacticUser.class);
+        factory().register(User.class);
+     }
+
+    public static Objectify ofy() {
+        return ObjectifyService.ofy();
+    }
+
+    public static ObjectifyFactory factory() {
+        return ObjectifyService.factory();
+    }
+}
