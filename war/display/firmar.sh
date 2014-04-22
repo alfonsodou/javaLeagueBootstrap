42909b6a2a06991b95de629b00f8107a0bba726f
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2014-04-22 15:19:41 +0200

    Problemas visualización partidos con applet

--- war/display/firmar.sh
+++ war/display/firmar.sh
@@ -1,5 +1,6 @@
 for f in *.jar;
 do
 	echo "Firmando $f ..";
-	jarsigner -keystore javaleague.keystore -storepass j4v4H1sp4n0 $f javaLeague;
+	echo "jarsigner -keystore javaleague.keystore -storepass j4v4H1sp4n0 $f javaLeague";
+	jarsigner -storepass j4v4H1sp4n0 $f javaleague;
 done;
