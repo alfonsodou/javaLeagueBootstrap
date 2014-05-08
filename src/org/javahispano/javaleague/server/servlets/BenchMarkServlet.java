9120092779d9b9aa3a1d39b4fae55099d21056a7
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2014-05-08 15:21:34 +0200

    Añadido servlet del benchMark

--- src/org/javahispano/javaleague/server/servlets/BenchMarkServlet.java
+++ src/org/javahispano/javaleague/server/servlets/BenchMarkServlet.java
@@ -3,7 +3,13 @@
  */
 package org.javahispano.javaleague.server.servlets;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author adou
@@ -15,5 +21,36 @@ public class BenchMarkServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException, ServletException {
+	    long t1 = System.nanoTime();
+
+	    int result = 0;
+	    for (int i = 0; i < 1000 * 1000; i++) {    // sole loop
+	        result += sum();
+	    }
+
+	    long t2 = System.nanoTime();
+	    
+	    PrintWriter out = resp.getWriter();
+		out.println("Execution time: " + ((t2 - t1) * 1e-9) +
+		        " seconds to compute result = " + result);
+		resp.flushBuffer();
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		doGet(req, res);
+	}
+	
+	private static int sum() {
+	    int sum = 0;
+	    for (int j = 0; j < 10 * 1000; j++) {
+	        sum += j;
+	    }
+	    return sum;
+	}	
 
 }
--- src/org/javahispano/javaleague/server/servlets/TimeTacticMatchServlet.java
+++ src/org/javahispano/javaleague/server/servlets/TimeTacticMatchServlet.java
@@ -5,6 +5,7 @@ package org.javahispano.javaleague.server.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -12,11 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.javahispano.javaleague.server.LoginHelper;
 import org.javahispano.javaleague.server.domain.MatchDAO;
-import org.javahispano.javaleague.server.domain.UserDAO;
 import org.javahispano.javaleague.shared.domain.Match;
-import org.javahispano.javaleague.shared.domain.User;
 
 /**
  * @author adou
@@ -47,14 +45,14 @@ public class TimeTacticMatchServlet extends HttpServlet {
 		PrintWriter out = res.getWriter();
 		if (req.getParameter("tactic").equals("local")) {
 			time = match.getTimeLocal();
-			res.setHeader("Content-disposition", "attachment; filename="
-					+ match.getId().toString() + "_" + match.getNameLocal()
-					+ ".csv");
+			res.setHeader("Content-disposition",
+					"attachment; filename=" + match.getId().toString() + "_"
+							+ eliminaBlancos(match.getNameLocal()) + ".csv");
 		} else {
 			time = match.getTimeVisita();
-			res.setHeader("Content-disposition", "attachment; filename="
-					+ match.getId().toString() + "_" + match.getNameForeign()
-					+ ".csv");
+			res.setHeader("Content-disposition",
+					"attachment; filename=" + match.getId().toString() + "_"
+							+ eliminaBlancos(match.getNameForeign()) + ".csv");
 		}
 
 		for (int i = 0; i < time.length; i++) {
@@ -68,5 +66,15 @@ public class TimeTacticMatchServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
 		doGet(req, res);
+	}
+
+	private String eliminaBlancos(String sTexto) {
+		StringTokenizer stTexto = new StringTokenizer(sTexto);
+		String sCadenaSinBlancos = "";
+
+		while (stTexto.hasMoreElements())
+			sCadenaSinBlancos += stTexto.nextElement();
+
+		return sCadenaSinBlancos;
 	}
 }
