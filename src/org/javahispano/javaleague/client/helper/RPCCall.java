2663fd6312f8608e097a0010089b52595539f1d6
Alfonso Dou Oblanca <alfonsodou@gmail.com>
2014-06-23 15:25:00 +0200

    Formateado fichero

--- src/org/javahispano/javaleague/client/helper/RPCCall.java
+++ src/org/javahispano/javaleague/client/helper/RPCCall.java
@@ -18,62 +18,65 @@ import com.google.gwt.user.client.rpc.SerializationException;
 /**
  * 
  * @author adou
- *
+ * 
  * @param <T>
  */
 public abstract class RPCCall<T> implements AsyncCallback<T> {
 
-  protected abstract void callService(AsyncCallback<T> cb);
+	protected abstract void callService(AsyncCallback<T> cb);
 
-  private void call(final int retriesLeft) {
-    onRPCOut();
+	private void call(final int retriesLeft) {
+		onRPCOut();
 
-    callService(new AsyncCallback<T>() {
-      public void onFailure(Throwable caught) {
-        onRPCIn();
-        GWT.log(caught.toString(), caught);
-        try {
-          throw caught;
-        } catch (InvocationException invocationException) {
-          if(caught.getMessage().equals(JavaLeagueConstants.LOGGED_OUT)){
-                JavaLeagueApp.get().getEventBus().fireEvent(new LogoutEvent());
-                return;
-          }
-          
-          if (retriesLeft <= 0) {
-            RPCCall.this.onFailure(invocationException);
-          } else {
-            call(retriesLeft - 1); // retry call
-          }
-        } catch (IncompatibleRemoteServiceException remoteServiceException) {
-          Window.alert("The app maybe out of date. Reload this page in your browser.");
-        } catch (SerializationException serializationException) {
-          Window.alert("A serialization error occurred. Try again.");
-        } catch (NotLoggedInException e) {
-          JavaLeagueApp.get().getEventBus().fireEvent(new LogoutEvent());
-        } catch (RequestTimeoutException e) {
-          Window.alert("This is taking too long, try again");
-        } catch (Throwable e) {// application exception
-          RPCCall.this.onFailure(e);
-        }
-      }
+		callService(new AsyncCallback<T>() {
+			public void onFailure(Throwable caught) {
+				onRPCIn();
+				GWT.log(caught.toString(), caught);
+				try {
+					throw caught;
+				} catch (InvocationException invocationException) {
+					if (caught.getMessage().equals(
+							JavaLeagueConstants.LOGGED_OUT)) {
+						JavaLeagueApp.get().getEventBus()
+								.fireEvent(new LogoutEvent());
+						return;
+					}
 
-      public void onSuccess(T result) {
-        onRPCIn();
-        RPCCall.this.onSuccess(result);
-      }
-    });
-  }
+					if (retriesLeft <= 0) {
+						RPCCall.this.onFailure(invocationException);
+					} else {
+						call(retriesLeft - 1); // retry call
+					}
+				} catch (IncompatibleRemoteServiceException remoteServiceException) {
+					Window.alert("The app maybe out of date. Reload this page in your browser.");
+				} catch (SerializationException serializationException) {
+					Window.alert("A serialization error occurred. Try again.");
+				} catch (NotLoggedInException e) {
+					JavaLeagueApp.get().getEventBus()
+							.fireEvent(new LogoutEvent());
+				} catch (RequestTimeoutException e) {
+					Window.alert("This is taking too long, try again");
+				} catch (Throwable e) {// application exception
+					RPCCall.this.onFailure(e);
+				}
+			}
 
-  private void onRPCIn() {
-    JavaLeagueApp.get().getEventBus().fireEvent(new RPCInEvent());
-  }
+			public void onSuccess(T result) {
+				onRPCIn();
+				RPCCall.this.onSuccess(result);
+			}
+		});
+	}
 
-  private void onRPCOut() {
-    JavaLeagueApp.get().getEventBus().fireEvent(new RPCOutEvent());
-  }
+	private void onRPCIn() {
+		JavaLeagueApp.get().getEventBus().fireEvent(new RPCInEvent());
+	}
 
-  public void retry(int retryCount) {
-    call(retryCount);
-  }
+	private void onRPCOut() {
+		JavaLeagueApp.get().getEventBus().fireEvent(new RPCOutEvent());
+	}
+
+	public void retry(int retryCount) {
+		call(retryCount);
+	}
 }
